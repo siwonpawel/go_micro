60abb0732250ebf088c647032c35617b7fa533ed
Paweł Siwoń <siwonpawel@gmail.com>
2022-07-30 20:53:48 +0200

    log authentication requests to logger-service

--- authentication-service/cmd/api/handlers.go
+++ authentication-service/cmd/api/handlers.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
 	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -28,6 +31,12 @@ func (app *Config) Authenticate(w http.ResponseWriter, r *http.Request) {
 		app.errorJSON(w, errors.New("invalid credentials"), http.StatusBadRequest)
 		return
 	}
+	err = app.logRequest("authentication", fmt.Sprintf("%s logged in", user.Email))
+	if err != nil {
+		log.Println("Error logging authentication event")
+		app.errorJSON(w, err)
+		return
+	}
 
 	payload := jsonResponse{
 		Error:   false,
@@ -36,4 +45,31 @@ func (app *Config) Authenticate(w http.ResponseWriter, r *http.Request) {
 	}
 
 	app.writeJSON(w, http.StatusAccepted, payload)
+}
+
+func (app *Config) logRequest(name, data string) error {
+	entry := struct {
+		Name string `json:"name"`
+		Data string `json:"data"`
+	}{
+		Name: name,
+		Data: data,
+	}
+
+	jsonData, _ := json.MarshalIndent(entry, "", "\t")
+	logServiceUrl := "http://logger-service/log"
+
+	request, err := http.NewRequest("POST", logServiceUrl, bytes.NewBuffer(jsonData))
+	if err != nil {
+		log.Printf("Error logging event: %d", err.Error())
+		return err
+	}
+
+	client := &http.Client{}
+	_, err = client.Do(request)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
